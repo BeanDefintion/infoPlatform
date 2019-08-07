be6635e20c35626f34edbc0747f889b4454b2cbf
田帅 <784700109@qq.com>
2019-08-07 18:15:55 +0800

    增加了eureka需要密码登陆

---
+++ infoplatform_eureka/src/main/java/com/platform/eureka/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.platform.eureka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * WebSecurityConfig
+ *
+ * @author 15293
+ * @version 1.0
+ * @since 2019/8/7 18:02
+ **/
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().httpBasic();
+        super.configure(http);
+    }
+}

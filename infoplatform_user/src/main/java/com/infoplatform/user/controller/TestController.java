240e0985f04850605372f1eedc296cb55b27fdff
田帅 <784700109@qq.com>
2019-08-08 18:39:53 +0800

    SpringBoot2更改暴露的端点

--- infoplatform_user/src/main/java/com/infoplatform/user/controller/TestController.java
+++ infoplatform_user/src/main/java/com/infoplatform/user/controller/TestController.java
@@ -4,6 +4,7 @@ import com.api.common.enums.StatusCode;
 import com.api.common.response.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1.0
  * @since 2019/8/8 14:08
  **/
+@RefreshScope
 @RestController
 @RequestMapping("test")
 public class TestController {
